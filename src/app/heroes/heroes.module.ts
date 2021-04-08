58db269f38bb9945758e85952ce9ad8021eb84b2
Marcos <marcos.ledesma.picazo@cieep.com>
2021-04-08 11:56:56 +0200

    Añadido pipe imagen

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -10,12 +10,13 @@ import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { ImagenPipe } from './pipes/imagen.pipe';
 
 
 
 
 @NgModule({
-  declarations: [ListadoComponent, AgregarComponent, BuscarComponent, HeroeComponent, HomeComponent, HeroeTarjetaComponent ],
+  declarations: [ListadoComponent, AgregarComponent, BuscarComponent, HeroeComponent, HomeComponent, HeroeTarjetaComponent, ImagenPipe ],
   imports: [
     CommonModule,
     HeroesRoutingModule,
